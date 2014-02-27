49865e3f5c4baa467efdb475210a7e380e479838
Mark McWilliams <mark@blep.net>
2014-02-27 07:34:51 -0800

    Setup script now installs bundles; updated .gitignore

--- setup.sh
+++ setup.sh
@@ -6,4 +6,9 @@ ln -s $SCRIPTPATH/vim/ $HOME/.vim
 ln -s $SCRIPTPATH/vim/vimrc $HOME/.vimrc
 ln -s $SCRIPTPATH/vim/gvimrc $HOME/.gvimrc
 
+git clone https://github.com/gmarik/vundle.git $SCRIPTPATH/vim/bundle/vundle
+
 vim +BundleInstall +qall
+
+cd $SCRIPTPATH/vim/bundle/YouCompleteMe
+install.sh
