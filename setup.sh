038213cc3b37d4db5d81eccf229c0ec0c85d708a
Mark McWilliams <mark@blep.net>
2014-02-27 07:05:35 -0800

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s $SCRIPTPATH/zshrc $HOME/.zshrc
 ln -s $SCRIPTPATH/vim/ $HOME/.vim
 ln -s $SCRIPTPATH/vim/vimrc $HOME/.vimrc
 ln -s $SCRIPTPATH/vim/gvimrc $HOME/.gvimrc
+
+vim +BundleInstall +qall
